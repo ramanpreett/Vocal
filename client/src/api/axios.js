e0f48af447534d184faf20622ebd10b468510e4a
Ramanpreet Kaur <ramanpreetkaurr04@gmail.com>
2026-06-15 13:15:43 +0530

    15-06-26

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001',
 });
 
 // Add a request interceptor to add the JWT token to requests
